6b9c9ca78f6c880a454273b03f7858a77a44a210
Ishara <isharadilshandev@gmail.com>
2020-08-04 09:45:30 +0530

    task: added colorbox component

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import ColorBox from './components/ColorBox';
 
 const App = () => {
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <Text>This is my first !!</Text>
+        <Text style={styles.headerTex}>
+          Here are some boxes of different colors
+        </Text>
+        <ColorBox color={'#2aa198'} />
+        <ColorBox color={'#268bd2'} />
+        <ColorBox color={'#d33682'} />
+        <ColorBox color={'#cb4b16'} />
       </View>
     </SafeAreaView>
   );
@@ -13,7 +20,36 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'red',
+    paddingTop: 40,
+    paddingHorizontal: 10,
+  },
+  headerTex: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  box: {
+    margin: 5,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  boxText: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
   },
 });
 export default App;
